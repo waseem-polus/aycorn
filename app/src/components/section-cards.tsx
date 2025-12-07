a56222a7cfb7c566b523db76329f7ed50546f6ed
Waseem Polus <69316929+waseem-polus@users.noreply.github.com>
2025-12-06 18:00:36 -0600

    dashboard initial commit

---
+++ app/src/components/section-cards.tsx
@@ -0,0 +1,113 @@
+import { IconTrendingUp } from "@tabler/icons-react";
+
+import { Badge } from "@/components/ui/badge";
+import {
+    Card,
+    CardAction,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+export function SectionCards() {
+    return (
+        <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+            <Card className="@container/card">
+                <CardHeader>
+                    <CardDescription>Total Active Projects</CardDescription>
+                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                        5
+                    </CardTitle>
+                    <CardAction>
+                        <Badge variant="outline">
+                            <IconTrendingUp />
+                            +25%
+                        </Badge>
+                    </CardAction>
+                </CardHeader>
+                <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                    <div className="line-clamp-1 flex gap-2 font-medium">
+                        New project added this quarter{" "}
+                        <IconTrendingUp className="size-4" />
+                    </div>
+                    <div className="text-muted-foreground">
+                        Software delivery demand increasing
+                    </div>
+                </CardFooter>
+            </Card>
+
+            <Card className="@container/card">
+                <CardHeader>
+                    <CardDescription>Completed Tasks</CardDescription>
+                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                        112
+                    </CardTitle>
+                    <CardAction>
+                        <Badge variant="outline">
+                            <IconTrendingUp />
+                            +12%
+                        </Badge>
+                    </CardAction>
+                </CardHeader>
+                <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                    <div className="line-clamp-1 flex gap-2 font-medium">
+                        Trending up this month
+                        <IconTrendingUp className="size-4" />
+                    </div>
+                    <div className="text-muted-foreground">
+                        Completed 12 tasks more than last month
+                    </div>
+                </CardFooter>
+            </Card>
+
+            <Card className="@container/card">
+                <CardHeader>
+                    <CardDescription>High Priority Tasks</CardDescription>
+                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                        4
+                    </CardTitle>
+                    <CardAction>
+                        <Badge variant="outline">
+                            <IconTrendingUp />
+                            +33%
+                        </Badge>
+                    </CardAction>
+                </CardHeader>
+                <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                    <div className="line-clamp-1 flex gap-2 font-medium">
+                        Critical workload growing{" "}
+                        <IconTrendingUp className="size-4" />
+                    </div>
+                    <div className="text-muted-foreground">
+                        Review resource allocation
+                    </div>
+                </CardFooter>
+            </Card>
+
+            <Card className="@container/card">
+                <CardHeader>
+                    <CardDescription>Tasks Completed on Time</CardDescription>
+                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                        70%
+                    </CardTitle>
+                    <CardAction>
+                        <Badge variant="outline">
+                            <IconTrendingUp />
+                            +15%
+                        </Badge>
+                    </CardAction>
+                </CardHeader>
+                <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                    <div className="line-clamp-1 flex gap-2 font-medium">
+                        Better delivery predictability{" "}
+                        <IconTrendingUp className="size-4" />
+                    </div>
+                    <div className="text-muted-foreground">
+                        Teams are meeting sprint commitments more consistently
+                    </div>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+}
