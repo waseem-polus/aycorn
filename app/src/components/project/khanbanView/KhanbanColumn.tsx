cb9da098c85168cb7262c32d7081d267421368cd
Waseem Polus <69316929+waseem-polus@users.noreply.github.com>
2025-12-14 15:09:35 -0600

    khanban col count

--- app/src/components/project/khanbanView/KhanbanColumn.tsx
+++ app/src/components/project/khanbanView/KhanbanColumn.tsx
@@ -29,9 +29,12 @@ export function KhanbanColumn({
   const filteredTasks = useMemo(
     () =>
       Tasks.filter((task) => {
-        return task.Name.toLowerCase().includes(search.toLowerCase());
+        return (
+          task.Name.toLowerCase().includes(search.toLowerCase()) &&
+          task.Status === status
+        );
       }),
-    [search, Tasks],
+    [search, Tasks, status],
   );
 
   return (
@@ -40,84 +43,77 @@ export function KhanbanColumn({
         <span className="flex items-center gap-2">
           <TaskStatusIcon variant={status} />
           {status}
+          <Badge variant="outline" className="size-5">
+            {filteredTasks.length}
+          </Badge>
         </span>
         <span className="text-neutral-500 text-sm">{description}</span>
       </div>
       <ItemGroup className="h-full overflow-y-scroll flex flex-col gap-2 pr-2">
-        {filteredTasks.map((task) => {
-          if (task.Status !== status) {
-            return;
-          }
+        {filteredTasks.map((task) => (
+          <TaskProvider defaultState={task} key={task.ID}>
+            <TaskSideDrawer>
+              <Item
+                asChild
+                className="border border-neutral-200 rounded-lg w-full box-border"
+              >
+                <a>
+                  <ItemHeader className="flex justify-between">
+                    <TaskPriorityIcon variant={task.Priority} />
+                    <Badge variant="outline">
+                      <LandPlot className="size-2" />
+                      {task.ChecklistName}
+                    </Badge>
+                  </ItemHeader>
+                  <ItemContent>
+                    {task.Name !== "" ? (
+                      <ItemTitle>{task.Name}</ItemTitle>
+                    ) : (
+                      <ItemTitle className="text-neutral-400">
+                        New Task
+                      </ItemTitle>
+                    )}
+                  </ItemContent>
 
-          return (
-            <TaskProvider defaultState={task} key={task.ID}>
-              <TaskSideDrawer>
-                <Item
-                  asChild
-                  className="border border-neutral-200 rounded-lg w-full box-border"
-                >
-                  <a>
-                    <ItemHeader className="flex justify-between">
-                      <TaskPriorityIcon variant={task.Priority} />
-                      <Badge variant="outline">
-                        <LandPlot className="size-2" />
-                        {task.ChecklistName}
+                  <ItemFooter>
+                    <span className="w-full flex flex-col gap-2">
+                      <TaskTypeBadge variant={task.Type} />
+                      <Badge
+                        variant={task.Assignee !== "" ? "secondary" : "outline"}
+                        className={
+                          task.Assignee !== "" ? "" : "text-neutral-500"
+                        }
+                      >
+                        <User className="size-2" />
+                        {task.Assignee === "" ? "Not Assigned" : task.Assignee}
                       </Badge>
-                    </ItemHeader>
-                    <ItemContent>
-                      {task.Name !== "" ? (
-                        <ItemTitle>{task.Name}</ItemTitle>
-                      ) : (
-                        <ItemTitle className="text-neutral-400">
-                          New Task
-                        </ItemTitle>
-                      )}
-                    </ItemContent>
-
-                    <ItemFooter>
-                      <span className="w-full flex flex-col gap-2">
-                        <TaskTypeBadge variant={task.Type} />
-                        <Badge
-                          variant={
-                            task.Assignee !== "" ? "secondary" : "outline"
-                          }
-                          className={
-                            task.Assignee !== "" ? "" : "text-neutral-500"
-                          }
-                        >
-                          <User className="size-2" />
-                          {task.Assignee === ""
-                            ? "Not Assigned"
-                            : task.Assignee}
-                        </Badge>
-                        <Badge
-                          variant={
-                            task.TimePlanned !== null ? "secondary" : "outline"
-                          }
-                          className={
-                            task.TimePlanned !== null ? "" : "text-neutral-500"
-                          }
-                        >
-                          <CalendarIcon className="size-2" />
-                          {task.TimePlanned !== null
-                            ? new Date(task.TimePlanned).toLocaleDateString(
-                                "en-US",
-                                {
-                                  year: "numeric",
-                                  month: "short",
-                                  day: "numeric",
-                                },
-                              )
-                            : "Not Scheduled"}
-                        </Badge>
-                      </span>
-                    </ItemFooter>
-                  </a>
-                </Item>
-              </TaskSideDrawer>
-            </TaskProvider>
-          );
-        })}
+                      <Badge
+                        variant={
+                          task.TimePlanned !== null ? "secondary" : "outline"
+                        }
+                        className={
+                          task.TimePlanned !== null ? "" : "text-neutral-500"
+                        }
+                      >
+                        <CalendarIcon className="size-2" />
+                        {task.TimePlanned !== null
+                          ? new Date(task.TimePlanned).toLocaleDateString(
+                              "en-US",
+                              {
+                                year: "numeric",
+                                month: "short",
+                                day: "numeric",
+                              },
+                            )
+                          : "Not Scheduled"}
+                      </Badge>
+                    </span>
+                  </ItemFooter>
+                </a>
+              </Item>
+            </TaskSideDrawer>
+          </TaskProvider>
+        ))}
       </ItemGroup>
     </div>
   );
