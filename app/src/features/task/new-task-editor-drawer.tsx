c055494822dd135b9b3871f0dad94d60a33ea289
Waseem Polus <69316929+waseem-polus@users.noreply.github.com>
2026-04-09 07:27:25 -0500

    move new task drawer trigger onClick into a useCallback

--- app/src/features/task/new-task-editor-drawer.tsx
+++ app/src/features/task/new-task-editor-drawer.tsx
@@ -4,9 +4,9 @@ import {
 } from "@/contexts/task/TaskContext";
 import { useTaskMutation } from "@/queries/useTaskMutation";
 import TaskEditorDrawer from "./task-editor-drawer";
-import { Button } from "../../components/ui/button";
+import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { ProjectContext } from "@/contexts/project/ProjectContext";
 import type { Task } from "@/types/types";
 
@@ -19,6 +19,22 @@ export function NewTaskEditorDrawer({
   const { Project, Checklists } = useContext(ProjectContext);
   const { create } = useTaskMutation(Project.ID);
 
+  const handleAddTask = useCallback(
+    () =>
+      create.mutate(
+        {
+          ...task,
+          Checklist: Checklists[0]?.ID,
+        },
+        {
+          onSuccess: (newTask: Task) => {
+            setTask(newTask);
+          },
+        },
+      ),
+    [create, task, Checklists, setTask],
+  );
+
   return (
     <TaskEditorDrawer
       onOpenChange={(open) => {
@@ -30,19 +46,7 @@ export function NewTaskEditorDrawer({
     >
       <Button
         className="bg-emerald-500 hover:bg-emerald-500 hover:cursor-pointer"
-        onClick={() =>
-          create.mutate(
-            {
-              ...task,
-              Checklist: Checklists[0]?.ID,
-            },
-            {
-              onSuccess: (newTask: Task) => {
-                setTask(newTask);
-              },
-            },
-          )
-        }
+        onClick={handleAddTask}
       >
         <Plus />
         New Task
