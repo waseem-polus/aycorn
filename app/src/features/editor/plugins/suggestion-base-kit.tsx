c82ee5ba730bf4c77a613619c9505073ea215ff0
Waseem Polus <69316929+waseem-polus@users.noreply.github.com>
2026-05-28 23:52:05 -0500

    remove unused discussion platejs plugin

--- app/src/features/editor/plugins/suggestion-base-kit.tsx
+++
@@ -1,7 +0,0 @@
-import { BaseSuggestionPlugin } from "@platejs/suggestion";
-
-import { SuggestionLeafStatic } from "@/components/ui/suggestion-node-static";
-
-export const BaseSuggestionKit = [
-  BaseSuggestionPlugin.withComponent(SuggestionLeafStatic),
-];
